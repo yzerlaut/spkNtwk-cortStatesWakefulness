ed4873feb3757eb270d27d738af90335d9ebb7df
Yann <yann.zerlaut@icm-institute.org>
2020-04-07 16:15:30 +0200

    update of TF plot

--- mean_field.py
+++ mean_field.py
@@ -6,11 +6,6 @@ import neural_network_dynamics.main as ntwk
 
 from model import Model, REC_POPS, AFF_POPS
 
-# by waiting for the proper params scan, let's pick those of the Cell Rep study
-for pop in REC_POPS:
-    Model['COEFFS_%s' % pop] = np.load('neural_network_dynamics/configs/The_Spectrum_of_Asynch_Dyn_2018/COEFFS_RecExc.npy')
-
-
 if sys.argv[-1]=='tf-sim':
     
     Model['filename'] = 'data/tf_data.npy'
@@ -35,8 +30,30 @@ elif sys.argv[-1]=='tf-fit':
 
     # from neural_network_dynamics.theory.fitting_tf import fit_data
     data['Model']['COEFFS'] = ntwk.fit_tf_data(data, order=2)
-    np.save('data/COEFFS_neuron.npy', data['Model']['COEFFS'])
+    np.save('data/COEFFS_pyrExc.npy', data['Model']['COEFFS'])
     
+    
+    
+elif sys.argv[-1]=='tf-sim-demo':
+
+    Model['filename'] = 'data/tf_data.npy'
+    Model['NRN_KEY'] = 'pyrExc' # we scan this population
+    
+    Model['tstop'] = 1200
+    Model['N_SEED'] = 1 # seed repetition
+    Model['SEED'] = 1 # seed
+    
+    Model['POP_STIM'] = ['pyrExc', 'recInh']
+    Model['F_pyrExc'] = 10.
+    Model['F_recInh'] = 2.
+
+    data = ntwk.run_single_cell_sim(Model, with_synaptic_currents=True, with_Vm=1, tdiscard=100)
+    fig, AX = ntwk.plot_single_cell_sim(data)
+    AX[-1].annotate('$\\nu_{e}$=%.1fHz, $\\nu_{i}$=%.1fHz    -->   $\\nu_{out}$=%.1fHz' %\
+                   (Model['F_pyrExc'], Model['F_recInh'], data['fout_mean']),
+                    (0.5,1), xycoords='axes fraction', ha='center')
+    fig.savefig('figures/tf_demo_sim.svg')
+    # ntwk.show()
     
 elif sys.argv[-1]=='tf-plot':
     
@@ -47,43 +64,81 @@ elif sys.argv[-1]=='tf-plot':
 
     from datavyz.main import graph_env
     ge = graph_env('manuscript')
-    fig, ax, acb = ge.figure(figsize=(1.5,1.2), bar_inset_loc=[1.1, 0., .05, 1.],
-                             right=5, bottom=0.8)
+    fig, ax, acb = ge.figure(figsize=(1.5,1.2),
+                             bar_inset_loc=[1.1, 0., .05, 1.],
+                             right=5, bottom=0.5, top=.4)
+
+    try:
+        data['Model']['COEFFS'] = np.load('data/COEFFS_pyrExc.npy')
+        with_theory=True
+    except FileNotFoundError:
+        with_theory=False
+        pass
+
+
+    # translation into total excitatory and inhibitory frequencies
+    data['F_recInh'] *= data['Model']['N_recInh']*data['Model']['p_recInh_pyrExc']
+    data['F_pyrExc'] *= data['Model']['N_pyrExc']*data['Model']['p_pyrExc_pyrExc']
+    data['Model']['N_recInh'] = 1
+    data['Model']['p_recInh_pyrExc'] = 1.
+    data['Model']['N_pyrExc'] = 1
+    data['Model']['p_pyrExc_pyrExc'] = 1
     
     ntwk.make_tf_plot_2_variables(data,
-                                  ge=ge, ax=ax, acb=acb,
+                                  with_theory=with_theory,
                                   xkey='F_pyrExc', ckey='F_recInh',
-                                  ylim=[1e-2, 80], yticks=[0.01, 0.1, 1, 10], yticks_labels=['<0.01', '0.1', '1', '10'], ylabel='$\\nu_{out}$ (Hz)',
-                                  xlim=[2, 100], xticks=[2, 5, 10, 20, 50], xticks_labels=['2', '5', '10', '20', '50'], xlabel='$\\nu_{e}$ (Hz)',
-                                  fig_args={'figsize':(1.5,1.5), 'with_space_for_bar_legend':True})
+                                  ylim=[1e-2, 100], yticks=[0.01, 0.1, 1, 10, 100],
+                                  yticks_labels=['<0.01', '0.1', '1', '10', '100'],
+                                  ylabel='$\\nu_{out}$ (Hz)',
+                                  xlim=[70, 3000],
+                                  xticks=[100, 200, 1000, 2000],
+                                  xticks_labels=['100', '200', '1000', '2000'],
+                                  xlabel='$\\nu_{e}^{tot}$ (Hz)',
+                                  ckey_label='$\\nu_{i}^{tot}$ (Hz)',
+                                  ge=ge, ax=ax, acb=acb)
+    # fig.savefig('figures/tf_final_plot.svg')
+
+
+    # from datavyz.plot_export import put_list_of_figs_to_svg_fig
+    # put_list_of_figs_to_svg_fig(['figures/tf_demo_sim.svg',
+    #                              'figures/tf_final_plot.svg'],
+    #                             fig_name="figures/tf.svg")
+    
     ntwk.show()
     
     
 else:
-    
-    from graphs.my_graph import graphs
-    mg = graphs()
+
+    from datavyz.main import graph_env
+    ge = graph_env()
 
     tstop, dt = 1, 5e-4
 
-    DYN_SYSTEM = {
-        'Exc': {'aff_pops':AFF_POPS, 'aff_pops_input_values':[3.], 'x0':1.},
-        'RecInh': {'aff_pops':['AffExc'], 'aff_pops_input_values':[3.], 'x0':1.}
-    }
-    INPUTS = {
-        'AffExc_RecExc':np.ones(int(tstop/dt))*3.,
-        'AffExc_RecInh':np.ones(int(tstop/dt))*3.
-        }
+    DYN_SYSTEM = {}
+    for pop in REC_POPS:
+        DYN_SYSTEM[pop] = {'aff_pops':AFF_POPS,
+                           'aff_pops_input_values':[3., 1.],
+                           'x0':0.}
+        Model['COEFFS_%s' % pop] = np.load('data/COEFFS_pyrExc.npy')
+        
+    # DYN_SYSTEM = {
+    #     'Exc': {'aff_pops':AFF_POPS, 'aff_pops_input_values':[3.], 'x0':1.},
+    #     'RecInh': {'aff_pops':['AffExc'], 'aff_pops_input_values':[3.], 'x0':1.}
+    # }
+    # INPUTS = {
+    #     'AffExc_RecExc':np.ones(int(tstop/dt))*3.,
+    #     'AffExc_RecInh':np.ones(int(tstop/dt))*3.
+    #     }
         
     tstop, dt = 2, 5e-4
     t = np.arange(int(tstop/dt))*dt
 
-    DYN_SYSTEM = {
-        'Exc': {'aff_pops':AFF_POPS, 'x0':1.},
-        'oscillExc': {'aff_pops':AFF_POPS, 'x0':1.},
-        'PvInh': {'aff_pops':AFF_POPS, 'x0':1.},
-        'SstInh': {'aff_pops':AFF_POPS, 'x0':1.},
-        'VipInh': {'aff_pops':AFF_POPS, 'x0':1.}}
+    # DYN_SYSTEM = {
+    #     'Exc': {'aff_pops':AFF_POPS, 'x0':1.},
+    #     'oscillExc': {'aff_pops':AFF_POPS, 'x0':1.},
+    #     'PvInh': {'aff_pops':AFF_POPS, 'x0':1.},
+    #     'SstInh': {'aff_pops':AFF_POPS, 'x0':1.},
+    #     'VipInh': {'aff_pops':AFF_POPS, 'x0':1.}}
     INPUTS = {}
     for aff in AFF_POPS:
         for pop in REC_POPS:
@@ -91,7 +146,7 @@ else:
 
     CURRENT_INPUTS = {'oscillExc':190*(1+np.sin(2*np.pi*3.*t))} # very low current just for demo
     
-    X0 = mean_field.find_fp(Model,
+    X0 = ntwk.mean_field.find_fp(Model,
                             DYN_SYSTEM,
                             INPUTS=INPUTS,
                             CURRENT_INPUTS=CURRENT_INPUTS,
@@ -99,16 +154,16 @@ else:
                  tstop=tstop,
                  replace_x0=True)
     
-    X = mean_field.solve_mean_field_first_order(Model, DYN_SYSTEM,
+    X = ntwk.mean_field.solve_mean_field_first_order(Model, DYN_SYSTEM,
                                      INPUTS=INPUTS,
                                      CURRENT_INPUTS=CURRENT_INPUTS,
                                      dt=dt, tstop=tstop)
 
     # print(mean_field.get_full_statistical_quantities(Model, DYN_SYSTEM))
     
-    fig, ax = mg.figure()
-    ax.set_yscale('log')
-    mg.plot(Y=[X['Exc'], X['oscillExc'], X['PvInh'], X['SstInh'], X['VipInh']],
-            COLORS=[mg.g, mg.b, mg.r, mg.purple, mg.orange], ax=ax, axes_args=dict(yticks=[0.1,1.,10], ylim=[1e-3, 100]))
-    mg.show()
+    fig, ax = ge.figure()
+    ge.plot(Y=[X[pop] for pop in REC_POPS],
+            # axes_args=dict(yticks=[0.1,1.,10], ylim=[1e-3, 100]),
+            COLORS=[ge.g, ge.b, ge.r, ge.purple, ge.orange], ax=ax)
+    ge.show()
 
