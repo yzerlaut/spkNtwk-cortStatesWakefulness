e7da873191bcef7e2a03571d24b145e8fcdcb495
Yann <yann.zerlaut@icm-institute.org>
2020-04-10 15:30:00 +0200

    adding batch test

--- iterative_random_search.py
+++ iterative_random_search.py
@@ -1,5 +1,7 @@
-import itertools
+import itertools, os
 import numpy as np
+
+from analyz.workflow import filename_with_datetime
 
 from model import REC_POPS, AFF_POPS, Model
 
@@ -11,11 +13,29 @@ for i, j in itertools.product(range(len(REC_POPS)+len(AFF_POPS)), range(len(REC_
     GRID[i,j,:] = np.array(INITIAL_LIMS)
 
 
+class InterativeSearch:
+    """
+    relies on a system of subfolders in data/
+
+    data/batch1/
+    data/batch2/
+    ...
+
+    """
+    def __init__(self, previous_batch='batch_test', new_batch='batch_test'):
+
+        print(os.path.listdir(os.path.join('data', previous_batch)))
+
+        # filename_with_datetime(filename, folder='./', extension='')
+        # PREV_CONFIGS, PREV_RESIDUALS = np.load()
+
+        
+        
 def sort_random_configs(GRID, n=10000):
 
-    CONFIGS = np.zeros((n,len(REC_POPS)+len(AFF_POPS), len(REC_POPS)))
+    self.CONFIGS = np.zeros((n,len(REC_POPS)+len(AFF_POPS), len(REC_POPS)))
     for i, j in itertools.product(range(len(REC_POPS)+len(AFF_POPS)), range(len(REC_POPS))):
-        CONFIGS[:,i,j] = np.random.uniform(GRID[i,j,0], GRID[i,j,1], size=n)
+        self.CONFIGS[:,i,j] = np.random.uniform(GRID[i,j,0], GRID[i,j,1], size=n)
 
     return CONFIGS
 
@@ -28,7 +48,7 @@ def run_sim(CONFIGS):
 
 def save_config(CONFIGS, RESIDUAL):
 
-    np.save('data/Mscan/test.npy', np.array([CONFIGS, RESIDUAL]))
+    np.save('data/Mscan/test.npy', CONFIGS, RESIDUAL)
     
 def launch_search():
     try:
