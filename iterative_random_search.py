7e6c0c36d36f0bb239f29b326509c6797871952e
Yann <yann.zerlaut@icm-institute.org>
2020-04-10 15:04:19 +0200

    adding connectivity matrix scan

--- iterative_random_search.py
+++ iterative_random_search.py
@@ -1,10 +1,27 @@
 import itertools
+import numpy as np
+
+from model import REC_POPS, AFF_POPS, Model
+
+INITIAL_LIMS = [0,100]
+
+GRID = np.zeros((len(REC_POPS)+len(AFF_POPS), len(REC_POPS),2))
+INITIAL_GRID = 0*GRID
+for i, j in itertools.product(range(len(REC_POPS)+len(AFF_POPS)), range(len(REC_POPS))):
+    GRID[i,j,:] = np.array(INITIAL_LIMS)
 
 
+def sort_random_configs(GRID, n=10000):
 
-INITIAL_GRID = np.array([[1,100] for i in range])
+    CONFIGS = np.zeros((n,len(REC_POPS)+len(AFF_POPS), len(REC_POPS)))
+    for i, j in itertools.product(range(len(REC_POPS)+len(AFF_POPS)), range(len(REC_POPS))):
+        CONFIGS[:,i,j] = np.random.uniform(GRID[i,j,0], GRID[i,j,1], size=n)
 
+    return CONFIGS
 
+def save_config(CONFIGS):
+
+    
 def launch_search():
     try:
         while True:
@@ -12,4 +29,6 @@ def launch_search():
     except KeyboardInterrupt:
         print('Scan stopped')
 
-launch_search()        
+if __name__=='__main__':
+    # launch_search()        
+    sort_random_configs(GRID, n=1000)
