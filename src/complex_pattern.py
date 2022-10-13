de12c213a718e691665e9a6af88f657b30fc918c
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-10-13 16:55:05 +0200

    update complex pattern

--- src/complex_pattern.py
+++ src/complex_pattern.py
@@ -34,16 +34,16 @@ Model = {
     'p_AffExcBG_RecExc':0.1, 'p_AffExcBG_RecInh':0.1, 'p_AffExcBG_DsInh':0.075,
     'p_AffExcTV_RecExc':0.1, 'p_AffExcTV_RecInh':0.1, 'p_AffExcTV_DsInh':0.075,
     # complex pattern stimulation
-    'STIM-delay':0.4,
-    'STIM-synchrony':0.4,
-    'STIM-frequency':1./1.5, # Hz
+    'STIM-delay':400,
+    'STIM-synchrony':0.2,
+    'STIM-frequency':3, # Hz
     'STIM-width':10, # ms
     'STIM-SEED':3, # stimulus seed
     # backrgound afferent stimulation (Hz)
     'F_AffExcBG':4.,
     'BG-SEED':3, # background activity seed
     # simulation parameters (ms)
-    'dt':0.1, 'tstop':1000,
+    'dt':0.1, 'tstop':4000,
     'SEED':3, # connectivity seed
     ## ---------------------------------------------------------------------------------
     # === cellular properties (based on AdExp), population by population ===
@@ -62,43 +62,18 @@ Model = {
     ## ---------------------------------------------------------------------------------
 }
 
-StimPattern = {'indices':[], 'times':[]}
-Nrecruited = int(Model['STIM-synchrony']*Model['N_AffExcTV'])
-events = Model['STIM-delay']+np.cumsum(np.random.exponential(1000/Model['STIM-frequency'], 
-                    int(10*Model['STIM-frequency']*Model['tstop']/1000)))
-print(events)
-for event in events[events<Model['tstop']]:
-    StimPattern['times'] += list(event+np.random.randn(Nrecruited)*Model['STIM-width'])
-    StimPattern['indices'] += list(np.random.choice(range(Model['N_AffExcTV']), Nrecruited))
+def run_single_sim(Model):
 
+    StimPattern = {'indices':[], 'times':[]}
+    Nrecruited = int(Model['STIM-synchrony']*Model['N_AffExcTV'])
 
-if sys.argv[-1]=='plot':
+    np.random.seed(Model['STIM-SEED'])
 
-    ######################
-    ## ----- Plot ----- ##
-    ######################
-    
-    ## load file
-    data = ntwk.recording.load_dict_from_hdf5('data/complex_pattern.h5')
-
-    # plot input patterm
-    fig, ax = plt.subplots(1)
-    ax.set_title('stim. pattern')
-    ax.plot(data['AffExcTV_times'], data['AffExcTV_indices'], 'ko', ms=1)
-    ax.set_xlim([0, Model['tstop']])
-    ax.set_xlabel('time (ms)')
-    ax.set_ylabel('nrn ID')
-
-    # ## plot
-    fig, _ = ntwk.plots.raster_and_Vm_plot(data, smooth_population_activity=10.)
-    
-    plt.show()
-
-else:
-
-    #############################
-    ## ----- Simulation ------ ##
-    #############################
+    events = Model['STIM-delay']+np.cumsum(np.random.exponential(1000/Model['STIM-frequency'], 
+                        int(10*Model['STIM-frequency']*Model['tstop']/1000)))
+    for event in events[events<Model['tstop']]:
+        StimPattern['times'] += list(event+np.random.randn(Nrecruited)*Model['STIM-width'])
+        StimPattern['indices'] += list(np.random.choice(range(Model['N_AffExcTV']), Nrecruited))
 
     REC_POPS = ['RecExc', 'RecInh', 'DsInh']
     AFF_POPS = ['AffExcBG', 'AffExcTV']
@@ -152,11 +127,56 @@ else:
 
     NTWK['AffExcTV_indices'] = StimPattern['indices']
     NTWK['AffExcTV_times'] = StimPattern['times']
+
+    return NTWK
+
+if sys.argv[-1]=='plot':
+
+    ######################
+    ## ----- Plot ----- ##
+    ######################
+    
+    ## load file
+    data = ntwk.recording.load_dict_from_hdf5('data/complex_pattern.h5')
+
+    # plot input patterm
+    fig, ax = plt.subplots(1)
+    ax.set_title('stim. pattern')
+    ax.plot(data['AffExcTV_times'], data['AffExcTV_indices'], 'ko', ms=1)
+    ax.set_xlim([0, data['tstop']])
+    ax.set_xlabel('time (ms)')
+    ax.set_ylabel('nrn ID')
+
+    # ## plot
+    fig, _ = ntwk.plots.raster_and_Vm_plot(data, smooth_population_activity=10.)
+    
+    plt.show()
+
+elif sys.argv[-1]=='scan':
+
+    Model['data_folder'] = './data/'
+    Model['zip_filename'] = 'data/regime-seed-scan.zip'
+
+    ntwk.scan.run(Model,
+                  ['F_AffExcBG', 'BG-SEED'],
+                  [np.array([4,20]),
+                   np.arange(10)],
+                  run_single_sim,
+                  fix_missing_only=True,
+                  parallelize=True)
+
+
+else:
+
+    #####################################
+    ## ----- Example Simulation ------ ##
+    #####################################
+
+    NTWK = run_single_sim(Model)
     ntwk.recording.write_as_hdf5(NTWK, 
                                  ARRAY_KEYS=['AffExcTV_indices', 'AffExcTV_times'],
                                  filename='data/complex_pattern.h5')
 
     print('Results of the simulation are stored as:', 'data/complex_pattern.h5')
     print('--> Run \"python pattern_input_data.py plot\" to plot the results')
-    
     
