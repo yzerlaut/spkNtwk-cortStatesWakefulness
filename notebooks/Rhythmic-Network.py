54d9f54f444b90dcdbfe2d3f1c271a9cce47dc2c
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-05-20 18:17:02 +0200

    v0-rhythmic-model

--- notebooks/Rhythmic-Network.py
+++ notebooks/Rhythmic-Network.py
@@ -1,8 +1,21 @@
-"""
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
 
-"""
+# %%
 import sys
-sys.path.append('./neural_network_dynamics')
+sys.path.append('../neural_network_dynamics')
 
 import numpy as np
 import matplotlib.pylab as plt
@@ -18,33 +31,17 @@ Model = {
     ## UNIT SYSTEM is : ms, mV, pF, nS, pA, Hz (arbitrary and unconsistent, so see code)
     ## ---------------------------------------------------------------------------------
     # numbers of neurons in population
-    'N_TcExc':100, 
-    'N_ReInh':100, 
-    'N_L4Exc':100, 
-    'N_L23Exc':100, 
-    'N_AffExc':50,
+    'N_TcExc':100, 'N_ReInh':100, 'N_L4Exc':100, 'N_L23Exc':100, 
     # synaptic weights
-    'Q_TcExc_L4Exc':10., 
-    'Q_TcExc_ReInh':10., 
-    'Q_ReInh_TcExc':30., 
-    'Q_ReInh_ReInh':30., 
-    'Q_L4Exc_TcExc':10., 
-    'Q_L4Exc_L23Exc':2., 
-    'Q_AffExc_TcExc':10.,
+    'Q_TcExc_L4Exc':10., 'Q_TcExc_ReInh':10., 'Q_ReInh_TcExc':30., 
+    'Q_ReInh_ReInh':30., 'Q_L4Exc_TcExc':10., 'Q_L4Exc_L23Exc':2., 'Q_AffExc_TcExc':10.,
     # synaptic time constants
-    'Tsyn_Exc':5., 
-    'Tsyn_Inh':5., 
+    'Tsyn_Exc':5.,  'Tsyn_Inh':5., 
     # synaptic reversal potentials
-    'Erev_Exc':0., 
-    'Erev_Inh':-80., 
+    'Erev_Exc':0., 'Erev_Inh':-80., 
     # connectivity parameters
-    'p_TcExc_L4Exc':0.05, 
-    'p_TcExc_ReInh':0.05, 
-    'p_L4Exc_TcExc':0.05, 
-    'p_L4Exc_L23Exc':0.1, 
-    'p_ReInh_ReInh':0.08, 
-    'p_ReInh_TcExc':0.08, 
-    'p_AffExc_TcExc':0.1, 
+    'p_TcExc_L4Exc':0.05, 'p_TcExc_ReInh':0.05, 'p_L4Exc_TcExc':0.05,  'p_L4Exc_L23Exc':0.1,
+    'p_ReInh_ReInh':0.08,  'p_ReInh_TcExc':0.08, 'p_AffExc_TcExc':0.1, 
     # simulation parameters
     'dt':0.1, 'tstop': 3000., 'SEED':3, # low by default, see later
     ## ---------------------------------------------------------------------------------
@@ -68,17 +65,18 @@ Model = {
     'F_AffExc':30,
 }
 
+POP_KEYS = ['TcExc', 'ReInh', 'L4Exc','L23Exc']
 
-NTWK = ntwk.build.populations(Model, ['TcExc', 'ReInh', 'L4Exc','L23Exc'],
+NTWK = ntwk.build.populations(Model, POP_KEYS,
                               AFFERENT_POPULATIONS=['AffExc'],
                               with_raster=True,
                               with_pop_act=True,
                               with_Vm=10,
                               # with_synaptic_currents=True,
                               # with_synaptic_conductances=True,
-                              verbose=True)
+                              verbose=False)
 
-ntwk.build.recurrent_connections(NTWK, SEED=5, verbose=True)
+ntwk.build.recurrent_connections(NTWK, SEED=5, verbose=False)
 
 #######################################
 ########### AFFERENT INPUTS ###########
@@ -88,10 +86,10 @@ t_array = ntwk.arange(int(Model['tstop']/Model['dt']))*Model['dt']
 f_array = 0.*t_array+Model['F_AffExc']
 # f_array[t_array<100] = 
 # # # afferent excitation onto cortical excitation and inhibition
-for i, tpop in enumerate(['TcExc']): # both on excitation and inhibition
+for i, tpop in enumerate(['TcExc']):
     ntwk.stim.construct_feedforward_input(NTWK, tpop, 'AffExc',
                                           t_array, f_array,
-                                          verbose=True,
+                                          verbose=False,
                                           SEED=int(37+i)%37)
 
 ################################################################
@@ -102,23 +100,26 @@ ntwk.build.initialize_to_rest(NTWK)
 #####################
 ## ----- Run ----- ##
 #####################
-network_sim = ntwk.collect_and_run(NTWK, verbose=True)
+network_sim = ntwk.collect_and_run(NTWK, verbose=False)
 
-ntwk.recording.write_as_hdf5(NTWK, filename='thal-oscill.h5')
+ntwk.recording.write_as_hdf5(NTWK, filename='../data/thal-oscill.h5')
 
+# %%
 ## load file
-data = ntwk.recording.load_dict_from_hdf5('thal-oscill.h5')
+data = ntwk.recording.load_dict_from_hdf5('../data/thal-oscill.h5')
 
 # ## plot
-fig, _ = ntwk.plots.activity_plots(data, 
+fig, AX = ntwk.plots.activity_plots(data, 
                                    Vm_args=None,#{'lw':0.3, 'subsampling':4},
                                    pop_act_args=dict(smoothing=4,
                                                      subsampling=4))
+for i in range(4):
+    ntwk.plots.pt.annotate(AX[0], i*'\n'+POP_KEYS[i], (1,1), va='top', color=ntwk.plots.pt.tab10(i), fontsize=9)
 fig, ax = ntwk.plots.pt.figure(figsize=(4,12), top=0, left=0, bottom=0)
-ntwk.plots.few_Vm_plot(data, lw=0.5, vpeak=10, shift=100,
+ntwk.plots.few_Vm_plot(data, lw=1, vpeak=10, shift=100,
                        clip_spikes=True,
                        spike_style='-', ax=ax,
                        COLORS=[ntwk.plots.pt.tab10(i) for i in range(4)],
                        NVMS=[[0],[0],[0],[0,2,3]])
 
-plt.show()
+# %%
