08b51c1b4951ad9e067c227b077bbd355c82dcb4
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-05-27 18:22:28 +0200

    now-spectrum

--- notebooks/Rhythmic-Network.py
+++ notebooks/Rhythmic-Network.py
@@ -37,16 +37,19 @@ Model = {
     'N_L5Inh':200, 
     'N_L4Exc':1000, 
     'N_L4Inh':200, 
-    'N_L23Exc':100, 
-    'N_L23Inh':200, 
+    'N_L23Exc':4000, 
+    'N_L23Inh':1000, 
     'N_BgExc':100,
-    'N_ModExc':100,
-    # synaptic weights
+    'N_ModExc':200,
+    # synaptic weights: recurrent
     'Q_TcExc_ReInh':5., 'Q_TcExc_L5Exc':5., 'Q_TcExc_L5Inh':5., 'Q_TcExc_L4Exc':5., 'Q_TcExc_L4Inh':5., 
     'Q_ReInh_TcExc':15., 'Q_ReInh_ReInh':15., 
     'Q_L5Exc_TcExc':5., 
     'Q_L4Exc_L23Exc':4., 'Q_L4Exc_L4Inh':4., 
     'Q_L4Inh_L4Exc':10., 
+    "Q_L23Exc_L23Exc":2.0, "Q_L23Exc_L23Inh":2.0, 
+    "Q_L23Inh_L23Exc":10.0, "Q_L23Inh_L23Inh":10.0, 
+    # synaptic weights: afferent
     'Q_BgExc_TcExc':10.,
     'Q_ModExc_TcExc':5., 'Q_ModExc_L5Inh':5., 'Q_ModExc_L5Exc':5., 'Q_ModExc_ReInh':5.,
     # synaptic time constants
@@ -70,7 +73,7 @@ Model = {
     # --> Layer 4 cortical population (Exc)
     'L5Exc_Gl':10., 'L5Exc_Cm':200.,'L5Exc_Trefrac':3.,
     'L5Exc_El':-60., 'L5Exc_Vthre':-50., 'L5Exc_Vreset':-60., 'L5Exc_deltaV':2.5,
-    'L5Exc_a':0., 'L5Exc_b': 80, 'L5Exc_tauw':200.,
+    'L5Exc_a':0., 'L5Exc_b': 100, 'L5Exc_tauw':200.,
     # --> Layer 5 cortical population (Inh)
     'L5Inh_Gl':10., 'L5Inh_Cm':200.,'L5Inh_Trefrac':3.,
     'L5Inh_El':-60., 'L5Inh_Vthre':-50., 'L5Inh_Vreset':-60., 'L5Inh_deltaV':2.5,
@@ -78,7 +81,7 @@ Model = {
     # --> Layer 4 cortical population (Exc)
     'L4Exc_Gl':10., 'L4Exc_Cm':200.,'L4Exc_Trefrac':3.,
     'L4Exc_El':-60.,'L4Exc_Vthre':-50.,'L4Exc_Vreset':-60.,'L4Exc_deltaV':2.5,
-    'L4Exc_a':0., 'L4Exc_b': 0, 'L4Exc_tauw':200.,
+    'L4Exc_a':0., 'L4Exc_b': 100, 'L4Exc_tauw':200.,
     # --> Layer 4 cortical population (Inh)
     'L4Inh_Gl':10., 'L4Inh_Cm':200.,'L4Inh_Trefrac':3.,
     'L4Inh_El':-60.,'L4Inh_Vthre':-50.,'L4Inh_Vreset':-60.,'L4Inh_deltaV':2.5,
@@ -86,21 +89,27 @@ Model = {
     # --> Layer 23 cortical population (Exc)
     'L23Exc_Gl':10., 'L23Exc_Cm':200.,'L23Exc_Trefrac':3.,
     'L23Exc_El':-60.,'L23Exc_Vthre':-50.,'L23Exc_Vreset':-60.,'L23Exc_deltaV':2.5,
-    'L23Exc_a':0., 'L23Exc_b': 80, 'L23Exc_tauw':200.,
+    'L23Exc_a':0., 'L23Exc_b': 0, 'L23Exc_tauw':1e4,
+    # --> Layer 23 cortical population (Inh)
+    'L23Inh_Gl':10., 'L23Inh_Cm':200.,'L23Inh_Trefrac':3.,
+    'L23Inh_El':-60.,'L23Inh_Vthre':-53.,'L23Inh_Vreset':-60.,'L23Inh_deltaV':2.5,
+    'L23Inh_a':0., 'L23Inh_b': 0, 'L23Inh_tauw':200.,
     #
-    'REC_POPS':['TcExc', 'ReInh', 'L5Exc', 'L5Inh', 'L4Exc'],
+    'REC_POPS':['TcExc', 'ReInh', 'L5Exc', 'L5Inh', 'L4Exc', 'L4Inh', 'L23Exc', 'L23Inh'],
     'AFF_POPS':['BgExc', 'ModExc'],
     # External drives
     'F_BgExc':8.,
     # connectivity parameters: recurrent populations
-    'p_TcExc_L5Exc':0.05, 'p_TcExc_ReInh':0.05, 'p_TcExc_L4Exc':0.05, 'p_TcExc_L4Inh':0.05, 
+    'p_TcExc_L5Exc':0.05, 'p_TcExc_ReInh':0.05, 'p_TcExc_L4Exc':0.04, 'p_TcExc_L4Inh':0.05, 
     'p_ReInh_ReInh':0.05, 'p_ReInh_TcExc':0.2, 
     'p_L5Exc_TcExc':0.025, 'p_L5Exc_L5Exc':0.05, 'p_L5Exc_L5Inh':0.05, 
-    'p_L4Exc_L4Inh':0.025, 'p_L4Exc_L23Exc':0.025, 
-    'p_L4Inh_L4Exc':0.05, 
+    'p_L4Exc_L4Inh':0.01, 'p_L4Exc_L23Exc':0.01, 'p_L4Exc_L23Inh':0.01, 
+    'p_L4Inh_L4Exc':0.01, 'p_L4Inh_L4Inh':0.05, 
+    "p_L23Exc_L23Exc":0.05, "p_L23Exc_L23Inh":0.05, 
+    "p_L23Inh_L23Exc":0.05, "p_L23Inh_L23Inh":0.05, 
     # connectivity parameters: afferent populations
-    'p_BgExc_TcExc':0.1, 
-    'p_ModExc_TcExc':0.2, 'p_ModExc_ReInh':0.3, 'p_ModExc_L5Inh':0.2, 
+    'p_BgExc_TcExc':0.1, #'p_BgExc_L4Exc':0.05, 
+    'p_ModExc_TcExc':0.25, 'p_ModExc_ReInh':0.15, 'p_ModExc_L5Inh':0.05, 
 }
 
 t = ntwk.arange(int(Model['tstop']/Model['dt']))*Model['dt']
@@ -145,7 +154,7 @@ NTWK = ntwk.build.populations(Model, Model['REC_POPS'],
                               custom_membrane_equation = custom_membrane_equation,
                               with_raster=True,
                               with_pop_act=True,
-                              with_Vm=5,
+                              with_Vm=3,
                               verbose=False)
 
 ntwk.build.recurrent_connections(NTWK, SEED=5, verbose=False)
@@ -188,8 +197,10 @@ data = ntwk.recording.load_dict_from_hdf5('../data/thal-oscill.ntwk.h5')
 # ## plot
 fig, AX = ntwk.plots.activity_plots(data, tzoom=[200,Model['tstop']],
                                     axes_extents = dict(Aff=1, Raster=2, Rate=3, Vm=1),
-                                    Vm_args=dict(lw=0.5, spike_peak=10, subsampling=4),
+                                    Vm_args=dict(lw=0.5, spike_peak=10, subsampling=1),
                                     pop_act_args=dict(smoothing=4, subsampling=4))
 
+
+# %%
 
 # %%
